c32860fe7ed783e2866cfa1fd4f568fa409d735f
Jason Gayle <jasonblue1626@gmail.com>
2021-11-28 13:51:11 -0500

    Removed redundante created method

--- src/components/Process/Process.vue
+++ src/components/Process/Process.vue
@@ -5,35 +5,17 @@
   </article>
 </template>
 <script>
-import anime from "animejs";
 export default {
   name: "Process",
 
   // Props to be displayed for respective algorithm
   props: ["pid", "burstTime", "arrivalTime", "priority"],
-
-  created() {
-    console.log("Created process");
-    const timeline = anime.timeline({
-      duration: 3000,
-    });
-
-    timeline.add({
-      scaleY: 10,
-      scaleX: 10,
-    });
-
-    timeline.add({
-      scaleY: 0,
-      scaleX: 0,
-    });
-  },
 };
 </script>
 <style scoped>
 article {
   margin: 0 5%;
-  animation: pop-in 0.8s ease;
+  animation: pop-in 0.8s ease-in-out;
   transition: ease-in-out 0.5s;
 }
 
