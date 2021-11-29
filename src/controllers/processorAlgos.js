b31d01a83f31af2def4fe8863bd235f2cd40d4ee
Matthew Palmer <palmer.matthew4567@gmail.com>
2021-11-29 00:47:53 -0500

    Removed the Comments in FCFS

--- src/controllers/processorAlgos.js
+++ src/controllers/processorAlgos.js
@@ -118,64 +118,6 @@ export class FCFS extends ProcessorSchedulingAlgorithm {
     }
 
     return tmline;
-
-    // generateTimeline() {
-    //   // let firstX = 800;
-    //   // let testAnimationTimeline = [];
-    //   // this.processes.forEach((process) => {
-    //   //   firstX -= 100;
-    //   //   testAnimationTimeline.push(
-    //   //     {
-    //   //       targets: ".p" + process.pid,
-    //   //       translateX: firstX,
-    //   //       scaleY: 0.8,
-    //   //     },
-    //   //     {
-    //   //       targets: ".p" + process.pid,
-    //   //       direction: "alternate",
-    //   //       rotate: {
-    //   //         value: 360 * process.time,
-    //   //         duration: 500 * process.time,
-    //   //         easing: "linear",
-    //   //       },
-    //   //       borderRadius: 50,
-    //   //     },
-    //   //     {
-    //   //       targets: ".p" + process.pid,
-    //   //       translateX: 900,
-    //   //       direction: "alternate",
-    //   //       opacity: 0,
-    //   //       duration: 500,
-    //   //     }
-    //   //   );
-    //   // });
-    //   // console.log(testAnimationTimeline);
-    //   // return testAnimationTimeline;
-
-    //   var tmline = [];
-    //   var iteration = 1;
-    //   const distanceTOCPU = 450,
-    //     shift = 100;
-    //   // tmline.join(set_up);
-    //   // this.queuesort();
-    //   while (this.queue.length != 0) {
-    //     let element = this.dequeue();
-    //     let name = ".p" + String(element.pid);
-
-    //     let minitl = [];
-    //     minitl.push(enterProc(name, 1500, distanceTOCPU, 0.8));
-    //     for (let i = 0; i < this.queue.length; i++) {
-    //       minitl.push(
-    //         shiftinQueue(".p" + String(this.queue[i].pid), 500, iteration * shift)
-    //       );
-    //     }
-    //     minitl.push(inProc(name, element.burstTime));
-    //     minitl.push(leaveProcDisperse(name));
-    //     tmline = tmline.concat(minitl);
-    //     iteration++;
-    //   }
-
-    //   return tmline;
   }
 }
 
